342c42120646e2739dd3cf9d11689134c5e4ddf1
wfan <wfan@yahoo-corp.jp>
2019-12-11 19:34:35 +0900

    add /jars

--- docker/env.sh
+++ docker/env.sh
@@ -6,6 +6,8 @@ set -o pipefail
 # Base dirs
 export BASE_DIR="`git rev-parse --show-toplevel`"
 export DOCKER_DIR="${BASE_DIR}/docker"
+export JARS_DIR="${DOCKER_DIR}/jars"
+mkdir -p "${JARS_DIR}"
 
 ### ----------------------------------------------------------------
 # ZMS dirs
